3d058bfa564ae5160177e8dc5079ca558142b13d
wcg316 <henry.rem.rem@gmail.com>
2025-02-24 20:50:27 +0800

    create test subscriber's script

--- Assets/Golbal/InputManager.cs
+++ Assets/Golbal/InputManager.cs
@@ -13,6 +13,8 @@ public class InputManager : MonoBehaviour
 {
     public static InputManager Instance { get; private set; }
 
+    public event Action<Direction> OnMove;
+
     void Awake()
     {
         Initialize();
@@ -32,6 +34,21 @@ public class InputManager : MonoBehaviour
 
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            OnMove?.Invoke(Direction.Up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            OnMove?.Invoke(Direction.Down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            OnMove?.Invoke(Direction.Left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            OnMove?.Invoke(Direction.Right);
+        }
     }
 }
