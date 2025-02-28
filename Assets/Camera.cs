f1d6d7afe084e79fb9b9a7fb017ee56c626c9463
wcg316 <henry.rem.rem@gmail.com>
2025-02-28 23:43:18 +0800

    remove unnecessary comments

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -5,14 +5,11 @@ public class Camera : MonoBehaviour
     public Transform target;
     public Vector3 offget;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //target = GameObject.FindWithTag("Player").transform;
         offget = new Vector3(0, -1, -100);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (target.position.x > -10 && target.position.y > -10)
