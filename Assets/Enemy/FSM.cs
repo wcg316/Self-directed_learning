855d861aca0511110f0e74fae7c018d575bfcd5d
wcg316 <henry.rem.rem@gmail.com>
2025-05-01 17:20:08 +0800

    initialize enemy's FSM

--- Assets/Enemy/FSM.cs
+++ Assets/Enemy/FSM.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class FSM
+public class FSM<T>
 {
-    
+    IState<T> currentInstance;
+    Dictionary<T, IState<T>> instances = new();
+    public T CurrentState { get; private set; }
+
+    public void AddState(T state, IState<T> instance)
+    {
+        instances[state] = instance;
+    }
+
+    public void ChangeStateTo(T state)
+    {
+        currentInstance?.Exit();
+        currentInstance = instances[state];
+        CurrentState = state;
+        currentInstance.Enter();
+    }
+
+    public void Execute()
+    {
+        currentInstance?.Execute();
+    }
 }
