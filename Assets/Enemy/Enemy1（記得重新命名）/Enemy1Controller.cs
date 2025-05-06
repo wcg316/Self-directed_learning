c408cdc3d44e3ba342f4cd867883746230a30408
wcg316 <henry.rem.rem@gmail.com>
2025-05-06 22:34:39 +0800

    add Enemy1Controller

---
+++ Assets/Enemy/Enemy1（記得重新命名）/Enemy1Controller.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+namespace Enemy1
+{
+	public class Enemy1Controller : MonoBehaviour
+	{
+		FSM<State> Enemy1AI = new FSM<State>();
+
+		void Start()
+		{
+			LoadStates();
+		}
+
+		void LoadStates()
+		{
+			Transform playerTransform = PlayerController.Instance.transform;
+
+			Enemy1AI.AddState(State.Born, new BornState(transform, playerTransform));
+			Enemy1AI.AddState(State.Partol, new PatrolState(transform, playerTransform));
+			Enemy1AI.AddState(State.Chase, new ChaseState(transform, playerTransform));
+			Enemy1AI.AddState(State.Attack, new AttackState(transform, playerTransform));
+			Enemy1AI.AddState(State.Die, new DieState(transform, playerTransform));
+		}
+
+		void Update()
+		{
+			Enemy1AI.Execute();
+		}
+	}
+}
